b9e1909a4c44119a16f30e916eb0876cf9a44cbc
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-18 21:35:54 +0700

    Initial commit

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,21 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Muhammad Ashary on 18/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    
+    case interestingPhotos = "flickr.interestingness.getList"
+    
+}
+
+struct FlickrAPI {
+    
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    
+}
---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Muhammad Ashary on 18/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class PhotsViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
