c2686e3c9ab9758b7a3ffba726e4adc868ec5bd5
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-19 01:11:24 +0700

    Add photo model

---
+++ Photorama/Photo.swift
@@ -0,0 +1,25 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Muhammad Ashary on 19/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+    
+}
