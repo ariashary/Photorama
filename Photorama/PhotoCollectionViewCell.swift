f9ba4f20eaba3f27bc5638728433af9323a90039
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-19 20:25:02 +0700

    Add custom collection view cell method

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Muhammad Ashary on 19/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+}
+
+
