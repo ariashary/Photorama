28cbd719f4f93d8546dc271651d7182e429f2053
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-19 18:07:35 +0700

    Add download and display image data

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2019 M. Ashary. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotoResult {
     case success([Photo])
@@ -39,6 +48,36 @@ class PhotoStore {
         }
         
         return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            completion(result)
+        }
+        
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                
+                // Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        
+        return .success(image)
     }
     
 }
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -23,8 +23,25 @@ class PhotosViewController: UIViewController {
             switch PhotoResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
+                
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
+            }
+        }
+    }
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (ImageResult) -> Void in
+            
+            switch ImageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
             }
         }
     }
