d89c4bb05c0eb0c2847839963fbdef8ffd125913
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-19 18:12:06 +0700

    Implement main thread method after completion

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -36,7 +36,9 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         
         task.resume()
@@ -58,7 +60,9 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processImageRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         
         task.resume()
